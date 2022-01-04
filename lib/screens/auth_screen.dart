2c511f508d136a2fe0d6845678ee3cd75504f36f
Dhari <dmakwt@gmail.com>
2022-01-04 05:15:14 -0600

    Update auth_screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course_chat_app_starter/controller/auth.dart';
+import 'package:flutter_svg/svg.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -25,85 +26,73 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CPES'),
+        title: SvgPicture.asset(
+          'assets/cpes.svg',
+          width: 75,
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(32.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextFormField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Email Address',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Email Required';
-                      }
-                      return null;
-                    },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'Email Address',
                   ),
+                  keyboardType: TextInputType.emailAddress,
                 ),
-                Container(height: 20),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextFormField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: 'Password',
-                    ),
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password Required';
-                      }
-                      return null;
-                    },
+              ),
+              Container(height: 20),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: 'Password',
                   ),
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
                 ),
-                Container(height: 70),
-                Container(
-                  width: 120,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // 1
-                      Auth().login(
-                        context,
-                        _emailController.text,
-                        _passwordController.text,
-                      );
-                    },
-                    child: const Text('Login'),
-                  ),
+              ),
+              Container(height: 70),
+              Container(
+                width: 120,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 1
+                    Auth().login(
+                      context,
+                      _emailController.text,
+                      _passwordController.text,
+                    );
+                  },
+                  child: const Text('Login'),
                 ),
-                Container(height: 15),
-                Container(
-                  width: 120,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // 2
-                      Auth().signup(
-                        context,
-                        _emailController.text,
-                        _passwordController.text,
-                      );
-                    },
-                    child: const Text('Sign Up'),
-                  ),
-                )
-              ],
-            ),
+              ),
+              Container(height: 15),
+              Container(
+                width: 120,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 2
+                    Auth().signup(
+                      context,
+                      _emailController.text,
+                      _passwordController.text,
+                    );
+                  },
+                  child: const Text('Sign Up'),
+                ),
+              )
+            ],
           ),
         ),
       ),
